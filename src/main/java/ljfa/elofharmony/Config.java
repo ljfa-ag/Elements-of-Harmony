028f6e9ccc1cfb6b52aa025d95f1fef06ebef2bc
ljfa-ag <ljfa-ag@web.de>
2015-01-03 13:53:05 +0100

    Clarify config options for Poison Joke

--- src/main/java/ljfa/elofharmony/Config.java
+++ src/main/java/ljfa/elofharmony/Config.java
@@ -42,10 +42,12 @@ public class Config {
                 "Default: Slowness, Mining Fatigue, Nausea, Blindness, Hunger, Weakness, Poison, Wither", 0, 32).getIntList();
         pjAvgDurations = conf.get(CAT_POISONJOKE, "meanDurations",
                 new int[] {360, 400, 240, 200, 320, 400, 160, 180},
-                "Average durations in ticks of the effect", 0, Integer.MAX_VALUE).getIntList();
+                "Average durations in ticks of the effect\n" +
+                "These entries correspond to the potion IDs specified in the I:potionIDs option.", 0, Integer.MAX_VALUE).getIntList();
         pjMaxLevels = conf.get(CAT_POISONJOKE, "maxLevels",
                 new int[] {2, 3, 2, 1, 2, 2, 1, 1},
-                "Maximum level of the effects", 1, 5).getIntList();
+                "Maximum level of the effects\n" +
+                "These entries correspond to the potion IDs specified in the I:potionIDs option.", 1, 5).getIntList();
         pjNumEffects = pjPotionIDs.length;
         if(pjAvgDurations.length != pjNumEffects || pjMaxLevels.length != pjNumEffects) {
             LogHelper.error("The length of the lists for potion IDs, durations and levels should be the same");
