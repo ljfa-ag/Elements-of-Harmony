c3a177e0383ab434d3dc8a49b6b17784112a37d2
ljfa-ag <ljfa-ag@web.de>
2015-01-27 23:07:36 +0100

    Add to Ritual Table

--- src/main/java/ljfa/elofharmony/blocks/BlockRitualTable.java
+++ src/main/java/ljfa/elofharmony/blocks/BlockRitualTable.java
@@ -1,11 +1,14 @@
 package ljfa.elofharmony.blocks;
 
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
+import java.util.Random;
+
+import ljfa.elofharmony.tile.TileInventoryBase;
 import ljfa.elofharmony.tile.TileRitualTable;
+import ljfa.elofharmony.util.LjfaMathHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
@@ -17,10 +20,10 @@ public class BlockRitualTable extends EohBlock implements ITileEntityProvider {
         setStepSound(soundTypeWood);
     }
     
-    @Override
+    /*@Override
     public boolean isOpaqueCube() {
         return false;
-    }
+    }*/
 
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
@@ -29,8 +32,32 @@ public class BlockRitualTable extends EohBlock implements ITileEntityProvider {
     
     @Override
     public void breakBlock(World world, int x, int y, int z, Block block, int meta) {
-        super.breakBlock(world, x, y, z, block, meta);
+        TileEntity te = world.getTileEntity(x, y, z);
+        if(te instanceof TileInventoryBase)
+            ((TileInventoryBase)te).spillItems();
         world.removeTileEntity(x, y, z);
+        super.breakBlock(world, x, y, z, block, meta);
+    }
+    
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player,
+            int side, float fx, float fy, float fz) {
+        if(world.isRemote)
+            return true;
+        Random rand = world.rand;
+
+        double mean = 300.0, sigma = 20.0;
+        
+        double dist = mean + sigma * rand.nextGaussian();
+        double angle = 2 * Math.PI * rand.nextDouble();
+        
+        int tpx = (int)(dist * Math.sin(angle));
+        int tpz = (int)(dist * Math.cos(angle));
+        int tpy = world.getTopSolidOrLiquidBlock(tpx, tpz);
+        
+        player.setPositionAndUpdate(tpx + 0.5, tpy + 2.0, tpz + 0.5);
+        
+        return true;
     }
     
     /*@SideOnly(Side.CLIENT)
--- src/main/java/ljfa/elofharmony/tile/TileRitualTable.java
+++ src/main/java/ljfa/elofharmony/tile/TileRitualTable.java
@@ -1,7 +1,50 @@
 package ljfa.elofharmony.tile;
 
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemPotion;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.tileentity.TileEntity;
 
-public class TileRitualTable extends TileEntity {
+public class TileRitualTable extends TileInventoryBase {
+    @Override
+    public int getSizeInventory() {
+        return 1;
+    }
 
+    @Override
+    public String getInventoryName() {
+        return "TileRitualTable";
+    }
+    
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack stack) {
+        Item item = stack.getItem();
+        return item == Items.apple
+                || item == Items.feather
+                || item == Items.cake
+                || isPotionOfType(item, stack.getItemDamage(), Potion.moveSpeed.id)
+                || item == Items.diamond
+                || item == Items.nether_star;
+    }
+    
+    private boolean isPotionOfType(Item item, int damage, int potionID) {
+        if(!(item instanceof ItemPotion))
+            return false;
+        ItemPotion potion = (ItemPotion)item;
+        List<PotionEffect> effects = potion.getEffects(damage);
+        if(effects == null)
+            return false;
+        for(PotionEffect eff: effects) {
+            if(eff != null && eff.getPotionID() == potionID)
+                return true;
+        }
+        return false;
+    }
 }
