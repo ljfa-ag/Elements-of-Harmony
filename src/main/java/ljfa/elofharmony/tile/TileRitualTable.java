e4092f03dc35f7a10dcde56a4c68b4f021c12019
ljfa-ag <ljfa-ag@web.de>
2015-01-30 19:47:28 +0100

    Store challenger in ritual table

--- src/main/java/ljfa/elofharmony/tile/TileRitualTable.java
+++ src/main/java/ljfa/elofharmony/tile/TileRitualTable.java
@@ -1,6 +1,7 @@
 package ljfa.elofharmony.tile;
 
 import java.util.List;
+import java.util.UUID;
 
 import ljfa.elofharmony.challenges.Challenge;
 import ljfa.elofharmony.challenges.ChallengeGenerosity;
@@ -11,10 +12,13 @@ import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPotion;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 
 public class TileRitualTable extends TileInventoryBase {
+    public UUID challengerUUID = null;
+    
     public TileRitualTable() {
         super(1);
     }
@@ -51,11 +55,29 @@ public class TileRitualTable extends TileInventoryBase {
             
             if(ChallengeHandler.startChallenge(player, challenge, xCoord, yCoord, zCoord)) {
                 setInventorySlotContents(0, null);
+                challengerUUID = player.getUniqueID();
                 return true;
             } else
                 return false;
         }
         return false;
+    }
+    
+    @Override
+    public void writeToNBT(NBTTagCompound tag) {
+        super.writeToNBT(tag);
+        if(challengerUUID != null) {
+            tag.setLong("challengerUUIDMost", challengerUUID.getMostSignificantBits());
+            tag.setLong("challengerUUIDLeast", challengerUUID.getLeastSignificantBits());
+        }
+    }
+    
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+        super.readFromNBT(tag);
+        if(tag.hasKey("challengerUUIDMost")) {
+            challengerUUID = new UUID(tag.getLong("challengerUUIDMost"), tag.getLong("challengerUUIDLeast"));
+        }
     }
     
     private boolean isPotionOfType(Item item, int damage, int potionID) {
