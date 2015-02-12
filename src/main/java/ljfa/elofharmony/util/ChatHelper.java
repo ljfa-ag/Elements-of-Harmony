4ca575a6b4b9c4953f9dc94c118c438bb80f05ae
ljfa-ag <ljfa-ag@web.de>
2015-02-12 13:21:02 +0100

    Add to the ChatHelper

--- src/main/java/ljfa/elofharmony/util/ChatHelper.java
+++ src/main/java/ljfa/elofharmony/util/ChatHelper.java
@@ -1,10 +1,24 @@
 package ljfa.elofharmony.util;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
 
 public class ChatHelper {
     public static void toPlayer(EntityPlayer player, String msg) {
         player.addChatMessage(new ChatComponentText(msg));
+    }
+    
+    public static void toPlayerLocalized(EntityPlayer player, String key, Object... args) {
+        player.addChatMessage(new ChatComponentTranslation(key, args));
+    }
+    
+    public static void broadcast(String msg) {
+        MinecraftServer.getServer().addChatMessage(new ChatComponentText(msg));
+    }
+    
+    public static void broadcastLocalized(String key, Object... args) {
+        MinecraftServer.getServer().addChatMessage(new ChatComponentTranslation(key, args));
     }
 }
