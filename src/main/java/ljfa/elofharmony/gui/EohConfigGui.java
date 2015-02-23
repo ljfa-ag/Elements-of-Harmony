fa43478c34634c54c9d4df891d1c3570fd442f4b
ljfa-ag <ljfa-ag@web.de>
2015-02-23 22:12:36 +0100

    Fetch list of categories instead of hardcoded

--- src/main/java/ljfa/elofharmony/gui/EohConfigGui.java
+++ src/main/java/ljfa/elofharmony/gui/EohConfigGui.java
@@ -6,8 +6,8 @@ import java.util.List;
 import ljfa.elofharmony.Config;
 import ljfa.elofharmony.Reference;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraftforge.common.config.ConfigCategory;
 import net.minecraftforge.common.config.ConfigElement;
-import cpw.mods.fml.client.config.DummyConfigElement;
 import cpw.mods.fml.client.config.GuiConfig;
 import cpw.mods.fml.client.config.IConfigElement;
 
@@ -16,20 +16,13 @@ public class EohConfigGui extends GuiConfig {
         super(parent, getConfigElements(), Reference.MODID, false, false, "Elements Of Harmony mod configuration");
     }
     
-    /** Creates a list of config elements */
+    /** Compiles a list of config elements */
     public static List<IConfigElement> getConfigElements() {
-        ArrayList<IConfigElement> list = new ArrayList<IConfigElement>();
+        List<IConfigElement> list = new ArrayList<IConfigElement>();
         
-        //Add categories to config GUI
-        list.add(categoryElement(Config.CAT_GENERAL, "General", "elofharmony.configgui.ctgy.general"));
-        list.add(categoryElement(Config.CAT_POISONJOKE, "Poison Joke", "elofharmony.configgui.ctgy.poisonjoke"));
+        for(String name: Config.conf.getCategoryNames())
+            list.add(new ConfigElement<ConfigCategory>(Config.conf.getCategory(name)));
         
         return list;
-    }
-    
-    /** Creates a button linking to another screen where all options of the category are available */
-    private static IConfigElement categoryElement(String category, String name, String tooltip_key) {
-        return new DummyConfigElement.DummyCategoryElement(name, tooltip_key,
-                new ConfigElement(Config.conf.getCategory(category)).getChildElements());
     }
 }
