05f82016f1095dff70b8e21f21341dcbe2a94cae
ljfa-ag <ljfa-ag@web.de>
2015-02-24 18:36:28 +0100

    Give the player a compass direction hint

--- src/main/java/ljfa/elofharmony/challenges/ChallengeGenerosity.java
+++ src/main/java/ljfa/elofharmony/challenges/ChallengeGenerosity.java
@@ -6,6 +6,7 @@ import ljfa.elofharmony.inventory.SlotType;
 import ljfa.elofharmony.items.ItemElement.ElementType;
 import ljfa.elofharmony.items.ModItems;
 import ljfa.elofharmony.tile.TileRitualTable;
+import ljfa.elofharmony.util.ChatHelper;
 import ljfa.elofharmony.util.LjfaMathHelper;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.item.ItemStack;
@@ -46,7 +47,7 @@ public class ChallengeGenerosity extends Challenge {
     public void onStart() {
         World world = player.getEntityWorld();
         double mean = DEBUG_MODE ? 30.0 : 300.0;
-        double sigma = DEBUG_MODE ? 20.0 : 2.0;
+        double sigma = DEBUG_MODE ? 2.0 : 20.0;
         
         double dist = mean + sigma * LjfaMathHelper.stdTriangular(world.rand);
         double angle = 2 * Math.PI * world.rand.nextDouble();
@@ -64,6 +65,8 @@ public class ChallengeGenerosity extends Challenge {
         }
         player.playerNetServerHandler.setPlayerLocation(tpx + 0.5, tpy, tpz + 0.5, yaw, 0.0f);
         world.playSoundEffect(tpx + 0.5, tpy + 0.5, tpz + 0.5, "mob.endermen.portal", 1.0f, 1.0f);
+        ChatHelper.toPlayer(player, "You have to go " + getCompassDirection(2*Math.PI - angle) + " in order to find back.");
+        ChatHelper.toPlayer(player, "The moon rises in the East. Use it to ortientate yourself!");
     }
     
     @Override
@@ -85,7 +88,28 @@ public class ChallengeGenerosity extends Challenge {
         table.endChallenge();
     }
     
-    private static final boolean DEBUG_MODE = false;
+    private static String getCompassDirection(double angle) {
+        if(angle < 0.125 * Math.PI)
+            return "East";
+        else if(angle < 0.375 * Math.PI)
+            return "Northeast";
+        else if(angle < 0.625 * Math.PI)
+            return "North";
+        else if(angle < 0.875 * Math.PI)
+            return "Northwest";
+        else if(angle < 1.125 * Math.PI)
+            return "West";
+        else if(angle < 1.375 * Math.PI)
+            return "Southwest";
+        else if(angle < 1.625 * Math.PI)
+            return "South";
+        else if(angle < 1.875 * Math.PI)
+            return "Southeast";
+        else
+            return "East";
+    }
+    
+    private static final boolean DEBUG_MODE = true;
     
     private final TileRitualTable table;
     private final PlayerInvRestriction invRestr;
