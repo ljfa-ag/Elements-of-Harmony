ad495c2303851bb5f6c494062e2d902ece9c736c
ljfa-ag <ljfa-ag@web.de>
2015-02-25 13:18:38 +0100

    Use gradle to fill in the version number

--- src/main/java/ljfa/elofharmony/Reference.java
+++ src/main/java/ljfa/elofharmony/Reference.java
@@ -3,7 +3,7 @@ package ljfa.elofharmony;
 public class Reference {
     public static final String MODID = "elofharmony";
     public static final String MODNAME = "Elements Of Harmony";
-    public static final String VERSION = "0.1-prealpha";
+    public static final String VERSION = "@VERSION@";
     public static final String CLIENT_PROXY_CLASS = "ljfa.elofharmony.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "ljfa.elofharmony.proxy.CommonProxy";
     public static final String GUI_FACTORY_CLASS = "ljfa.elofharmony.gui.EohGuiFactory";
