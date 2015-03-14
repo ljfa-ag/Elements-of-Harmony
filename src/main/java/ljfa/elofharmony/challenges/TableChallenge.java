638de0af75d69ab8dcf298ee1de7967a974a5ff3
ljfa-ag <ljfa-ag@web.de>
2015-03-14 21:00:37 +0100

    Fix NPE

--- src/main/java/ljfa/elofharmony/challenges/TableChallenge.java
+++ src/main/java/ljfa/elofharmony/challenges/TableChallenge.java
@@ -39,7 +39,7 @@ public abstract class TableChallenge extends Challenge {
             EntityItem entity = new EntityItem(tablePos.getWorld(), tablePos.x+0.5, tablePos.y+0.5, tablePos.z+0.5, result);
             tablePos.getWorld().spawnEntityInWorld(entity);
         }
-        table.getWorldObj().playSoundEffect(tablePos.x+0.5, tablePos.y+0.5, tablePos.z+0.5, "random.levelup", 1.0f, 1.0f);
+        tablePos.getWorld().playSoundEffect(tablePos.x+0.5, tablePos.y+0.5, tablePos.z+0.5, "random.levelup", 1.0f, 1.0f);
     }
     
     @Override
