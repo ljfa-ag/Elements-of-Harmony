b7cb3f311529dee29934d302e734491799ed6aea
ljfa-ag <ljfa-ag@web.de>
2015-03-14 19:48:49 +0100

    Create GameUtils

---
+++ src/main/java/ljfa/elofharmony/util/GameUtils.java
@@ -0,0 +1,22 @@
+package ljfa.elofharmony.util;
+
+import net.minecraft.client.Minecraft;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class GameUtils {
+    
+    @SideOnly(Side.CLIENT)
+    public static int getRenderDistance() {
+        int chunks = Minecraft.getMinecraft().gameSettings.renderDistanceChunks;
+        if(chunks <= 0)
+            chunks = 10;
+        return 16 * chunks;
+    }
+    
+    @SideOnly(Side.CLIENT)
+    public static int getRenderDistanceSq() {
+        int dist = getRenderDistance();
+        return dist*dist;
+    }
+}
