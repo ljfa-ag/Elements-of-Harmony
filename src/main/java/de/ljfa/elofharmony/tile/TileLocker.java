f3a3122efec35bd6e4281fbc9581101740da5d37
ljfa-ag <ljfa-ag@web.de>
2015-04-07 17:50:10 +0200

    Only accept armor in armor slots

--- src/main/java/de/ljfa/elofharmony/tile/TileLocker.java
+++ src/main/java/de/ljfa/elofharmony/tile/TileLocker.java
@@ -1,5 +1,8 @@
 package de.ljfa.elofharmony.tile;
 
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import de.ljfa.lib.tile.TileInventoryBase;
 
 public class TileLocker extends TileInventoryBase {
@@ -7,6 +10,7 @@ public class TileLocker extends TileInventoryBase {
      *  0-35: inventory
      * 36-39: armor
      */
+    public static final int normalInvStart = 0, armorInvStart = 36;
     
     public TileLocker() {
         super(36 + 4);
@@ -17,4 +21,16 @@ public class TileLocker extends TileInventoryBase {
         return "TileLocker";
     }
 
+    @Override
+    public boolean isItemValidForSlot(int slot, ItemStack stack) {
+        if(slot < armorInvStart)
+            return true;
+        else {
+            Item item = stack.getItem();
+            if(item instanceof ItemArmor)
+                return ((ItemArmor)item).armorType == slot-armorInvStart;
+            else
+                return false;
+        }
+    }
 }
