e52518aace91fed5886b517b705a75626e915161
ljfa-ag <ljfa-ag@web.de>
2015-04-07 22:14:28 +0200

    Preliminary shift-click implementation

--- src/main/java/de/ljfa/elofharmony/inventory/ContainerLocker.java
+++ src/main/java/de/ljfa/elofharmony/inventory/ContainerLocker.java
@@ -3,11 +3,15 @@ package de.ljfa.elofharmony.inventory;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 import de.ljfa.elofharmony.tile.TileLocker;
 import de.ljfa.lib.inventory.ContainerBase;
 
 public class ContainerLocker extends ContainerBase {
 
+    private static final int playerHotbarStart = 0, playerInvStart = 9, playerArmorStart = 36;
+    private static final int lockerHotbarStart = 40, lockerInvStart = 49, lockerArmorStart = 76, endSlots = 80;
+    
     private final TileLocker tile;
     
     public ContainerLocker(InventoryPlayer invPlayer, TileLocker tile) {
@@ -30,6 +34,27 @@ public class ContainerLocker extends ContainerBase {
         //Locker armor slots
         for(int i = 0; i < 4; i++)
             addSlotToContainer(new Slot(tile, 36 + i, 8, 20 + 18*i));
+    }
+    
+    @Override
+    public ItemStack transferStackInSlot(EntityPlayer player, int slotInd) {
+        Slot slot = (Slot)inventorySlots.get(slotInd);
+        
+        if(slot != null && slot.getHasStack()) {
+            int corrSlotInd;
+            if(slotInd < lockerHotbarStart)
+                corrSlotInd = slotInd + lockerHotbarStart;
+            else
+                corrSlotInd = slotInd - lockerHotbarStart;
+            
+            Slot corrSlot = (Slot)inventorySlots.get(corrSlotInd);
+            if(corrSlot != null && !corrSlot.getHasStack()) {
+                ItemStack stackToTr = slot.getStack();
+                slot.putStack(null);
+                corrSlot.putStack(stackToTr);
+            }
+        }
+        return null;
     }
     
     @Override
