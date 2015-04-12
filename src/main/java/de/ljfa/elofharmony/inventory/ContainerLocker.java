612213bf3af9e82bc9a02e93e8403410e0d89e87
ljfa-ag <ljfa-ag@web.de>
2015-04-12 18:21:46 +0200

    Move boilerplate transferStackInSlot code to superclass

--- src/main/java/de/ljfa/elofharmony/inventory/ContainerLocker.java
+++ src/main/java/de/ljfa/elofharmony/inventory/ContainerLocker.java
@@ -9,8 +9,8 @@ import de.ljfa.lib.inventory.ContainerBase;
 
 public class ContainerLocker extends ContainerBase {
 
-    private static final int lockerInvStart = 0, lockerHotbarStart = 27, lockerArmorStart = 36, endSlots = 80;
-    private static final int playerInvStart = 40, playerHotbarStart = 67, playerArmorStart = 76;
+    private static final int lockerInvStart = 0, lockerHotbarStart = 27, lockerArmorStart = 36;
+    private static final int playerInvStart = 40, playerHotbarStart = 67, playerArmorStart = 76, endSlots = 80;
     
     private final TileLocker tile;
     
@@ -30,7 +30,7 @@ public class ContainerLocker extends ContainerBase {
         for(int i = 0; i < 4; i++)
             addSlotToContainer(new Slot(tile, 36 + i, 8, 20 + 18*i));
         
-        //Player inventory slots
+        //Player inventory and hotbar slots
         addPlayerInv(invPlayer, 30, 108);
         
         //Player armor slots
@@ -39,34 +39,18 @@ public class ContainerLocker extends ContainerBase {
     }
     
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer player, int slotInd) {
-        ItemStack copyStack = null;
-        Slot slot = (Slot)inventorySlots.get(slotInd);
-        
-        if(slot != null && slot.getHasStack()) {
-            ItemStack stackInSlot = slot.getStack();
-            copyStack = stackInSlot.copy();
-            
-            boolean isInLockerInv = slotInd < playerInvStart;
-            int corrSlotInd = slotInd + (isInLockerInv ? playerInvStart : -playerInvStart);
-            if(!mergeItemStack(stackInSlot, corrSlotInd, corrSlotInd+1, false)) {
-                int invStart = isInLockerInv ? playerInvStart : lockerInvStart;
-                int invEnd = isInLockerInv ? playerArmorStart : lockerArmorStart;
-                if(!mergeItemStack(stackInSlot, invStart, invEnd, true))
-                    return null;
-            }
-            
-            if(stackInSlot.stackSize == 0)
-                slot.putStack(null);
-            else
-                slot.onSlotChanged();
-            
-            if(copyStack.stackSize == stackInSlot.stackSize)
-                return null;
-            
-            slot.onPickupFromSlot(player, stackInSlot);
+    protected boolean doTransferStack(ItemStack stackInSlot, int slotInd) {
+        boolean isInLockerInv = slotInd < playerInvStart;
+        //Try to transfer into the corresponding slot on the other side first
+        int corrSlotInd = slotInd + (isInLockerInv ? playerInvStart : -playerInvStart);
+        if(!mergeItemStack(stackInSlot, corrSlotInd, corrSlotInd+1, false)) {
+            //Try to transfer into the rest of the inventory
+            int invStart = isInLockerInv ? playerInvStart : lockerInvStart;
+            int invEnd = isInLockerInv ? playerArmorStart : lockerArmorStart;
+            if(!mergeItemStack(stackInSlot, invStart, invEnd, true))
+                return false;
         }
-        return copyStack;
+        return true;
     }
     
     @Override
--- src/main/java/de/ljfa/lib/inventory/ContainerBase.java
+++ src/main/java/de/ljfa/lib/inventory/ContainerBase.java
@@ -16,10 +16,31 @@ public abstract class ContainerBase extends Container {
      * Else, a copy of the stack that was originally in the slot.
      */
     @Override
-    public ItemStack transferStackInSlot(EntityPlayer player, int slot) {
-        return null;
+    public ItemStack transferStackInSlot(EntityPlayer player, int slotInd) {
+        ItemStack copyStack = null;
+        Slot slot = (Slot)inventorySlots.get(slotInd);
+        
+        if(slot != null && slot.getHasStack()) {
+            ItemStack stackInSlot = slot.getStack();
+            copyStack = stackInSlot.copy();
+            
+            if(!doTransferStack(stackInSlot, slotInd))
+                return null;
+            
+            if(stackInSlot.stackSize == 0)
+                slot.putStack(null);
+            else
+                slot.onSlotChanged();
+            
+            if(copyStack.stackSize == stackInSlot.stackSize)
+                return null;
+            
+            slot.onPickupFromSlot(player, stackInSlot);
+        }
+        return copyStack;
     }
     
+    /** Adds the slots for the player inventory to the container at the given coordinates. */
     protected void addPlayerInv(InventoryPlayer invPlayer, int x, int y) {
         for(int i = 0; i < 3; i++)
             for(int j = 0; j < 9; j++)
@@ -28,4 +49,10 @@ public abstract class ContainerBase extends Container {
         for(int j = 0; j < 9; j++)
             addSlotToContainer(new Slot(invPlayer, j, x + 18*j, y + 58));
     }
+    
+    /**
+     * Does the actual transferring of the stack in the slot to somewhere else.
+     * @return true if the stack could be transferred at least partially
+     */
+    protected abstract boolean doTransferStack(ItemStack stack, int slot);
 }
