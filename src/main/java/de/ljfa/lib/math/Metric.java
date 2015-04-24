712a07c88837ff232940c3f52237fc325b199963
ljfa-ag <ljfa-ag@web.de>
2015-04-24 12:48:06 +0200

    Make fields explicitly public

--- src/main/java/de/ljfa/lib/math/Metric.java
+++ src/main/java/de/ljfa/lib/math/Metric.java
@@ -6,7 +6,7 @@ public interface Metric {
     double dist(double x1, double y1, double z1, double x2, double y2, double z2);
     
     /** The Manhattan metric. */
-    static final Metric l1 = new Metric() {
+    public static final Metric l1 = new Metric() {
         @Override
         public double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
             return Math.abs(x1-x2) + Math.abs(y1-y2) + Math.abs(z1-z2);
@@ -14,7 +14,7 @@ public interface Metric {
     };
     
     /** The square of the euclidean metric. This is not a metric itself. */
-    static final Metric l2sq = new Metric() {
+    public static final Metric l2sq = new Metric() {
         @Override
         public double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
             return (x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2);
@@ -22,7 +22,7 @@ public interface Metric {
     };
     
     /** The euclidean metric. */
-    static final Metric l2 = new Metric() {
+    public static final Metric l2 = new Metric() {
         @Override
         public double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
             return Math.sqrt(l2sq.dist(x1, y1, z1, x2, y2, z2));
@@ -30,7 +30,7 @@ public interface Metric {
     };
     
     /** The maximum norm metric. */
-    static final Metric lInf = new Metric() {
+    public static final Metric lInf = new Metric() {
         @Override
         public double dist(double x1, double y1, double z1, double x2, double y2, double z2) {
             return Math.max(Math.abs(x1-x2), Math.max(Math.abs(y1-y2), Math.abs(z1-z2)));
