ef93997fcb8e3ff7cd5075fd64acac604b34ee71
ljfa-ag <ljfa-ag@web.de>
2015-05-08 19:37:19 +0200

    Add vanillaTools to ItemHelper

--- src/main/java/de/ljfa/lib/items/ItemHelper.java
+++ src/main/java/de/ljfa/lib/items/ItemHelper.java
@@ -1,9 +1,14 @@
 package de.ljfa.lib.items;
 
+import java.util.Arrays;
+import java.util.Set;
+
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
+
+import com.google.common.collect.Sets;
 
 public class ItemHelper {
 
@@ -15,5 +20,16 @@ public class ItemHelper {
             return item == Items.skull || item == Item.getItemFromBlock(Blocks.pumpkin);
         else
             return false;
+    }
+    
+    /** Set of all vanilla pickaxes, shovels and axes */
+    public static final Set<Item> vanillaTools;
+    
+    static {
+        vanillaTools = Sets.newIdentityHashSet();
+        vanillaTools.addAll(Arrays.asList(
+            Items.wooden_pickaxe, Items.stone_pickaxe, Items.iron_pickaxe, Items.diamond_pickaxe,
+            Items.wooden_shovel, Items.stone_shovel, Items.iron_shovel, Items.diamond_shovel,
+            Items.wooden_axe, Items.stone_axe, Items.iron_axe, Items.diamond_axe));
     }
 }
