93a33b6427e20df7d23d2f01e0a4ace1cc0ff294
ljfa-ag <ljfa-ag@web.de>
2015-08-24 12:10:55 +0200

    Add block state property for running Ritual Table

--- src/main/java/de/ljfa/elofharmony/blocks/BlockRitualTable.java
+++ src/main/java/de/ljfa/elofharmony/blocks/BlockRitualTable.java
@@ -1,27 +1,31 @@
 package de.ljfa.elofharmony.blocks;
 
 import static de.ljfa.elofharmony.ElementsOfHarmony.logger;
+
+import de.ljfa.elofharmony.tile.TileRitualTable;
+import de.ljfa.lib.inventory.InvUtils;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import de.ljfa.elofharmony.Reference;
-import de.ljfa.elofharmony.tile.TileRitualTable;
-import de.ljfa.lib.inventory.InvUtils;
 
 public class BlockRitualTable extends BlockBase implements ITileEntityProvider {
-
+    public static final PropertyBool RUNNING = PropertyBool.create("running");
+    
     public BlockRitualTable() {
         super("ritual_table", Material.wood);
         setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 0.75f, 1.0f);
         setHardness(2.0f);
         setResistance(20.0f);
         setStepSound(soundTypeWood);
-        setDefaultState(blockState.getBaseState());
+        setDefaultState(blockState.getBaseState().withProperty(RUNNING, false));
     }
 
     @Override
@@ -53,4 +57,19 @@ public class BlockRitualTable extends BlockBase implements ITileEntityProvider {
         return false;
     }
 
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        return 0;
+    }
+    
+    @Override
+    public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+        TileRitualTable te = (TileRitualTable)world.getTileEntity(pos);
+        return state.withProperty(RUNNING, te.hasChallenge());
+    }
+    
+    @Override
+    protected BlockState createBlockState() {
+        return new BlockState(this, RUNNING);
+    }
 }
