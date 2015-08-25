f68e857e2b9e576611d1e18aef01e0416e397a8b
ljfa-ag <ljfa-ag@web.de>
2015-08-25 11:37:33 +0200

    Get item rendering on the ritual table to work

--- src/main/java/de/ljfa/elofharmony/render/TileRitualTableRenderer.java
+++ src/main/java/de/ljfa/elofharmony/render/TileRitualTableRenderer.java
@@ -1,58 +1,35 @@
 package de.ljfa.elofharmony.render;
 
+import org.lwjgl.opengl.GL11;
+
+import de.ljfa.elofharmony.tile.TileRitualTable;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.RenderItem;
-import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.entity.item.EntityItem;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
-import org.lwjgl.opengl.GL11;
-
-import de.ljfa.elofharmony.render.models.ModelRitualTable;
-import de.ljfa.elofharmony.tile.TileRitualTable;
-
 @SideOnly(Side.CLIENT)
 public class TileRitualTableRenderer extends TileEntitySpecialRenderer {
-    private final ModelRitualTable model;
-    //private final RenderItem renderItem;
-    
-    private final EntityItem shownItem;
-    
-    //private static final ResourceLocation texture = new ResourceLocation(Reference.MODID, "textures/tile/ritual_table.png");
-    //private static final ResourceLocation texture_running = new ResourceLocation(Reference.MODID, "textures/tile/ritual_table_running.png");
+    private final RenderItem renderItem;
     
     public TileRitualTableRenderer() {
-        model = new ModelRitualTable();
-        //renderItem = new RenderItem();
-        
-        shownItem = new EntityItem(null);
-        shownItem.hoverStart = 0.0f;
+        renderItem = Minecraft.getMinecraft().getRenderItem();
     }
     
     //destroyProgress: Ranges from 0 to 9 when the block gets dug, negative otherwise
     @Override
     public void renderTileEntityAt(TileEntity te, double x, double y, double z, float partialTick, int destroyProgress) {
-        TileRitualTable tile = (TileRitualTable)te;
-        
-        GL11.glPushMatrix();
-        GL11.glTranslated(x, y, z);
-        
-        /*if(tile.hasChallenge())
-            bindTexture(texture_running);
-        else
-            bindTexture(texture);
-        
-        model.renderAll();*/
-        
-        if(tile.getStackInSlot(0) != null) {
-            GL11.glTranslated(0.5, 0.63, 0.5);
-            shownItem.setEntityItemStack(tile.getStackInSlot(0));
-            //renderItem.doRender(shownItem, 0, 0, 0, 0, 0);
+        ItemStack stack = ((TileRitualTable)te).getStackInSlot(0);
+        if(stack != null) {
+            GL11.glPushMatrix();
+            GL11.glTranslated(x+0.5, y+0.75, z+0.5);
+            GL11.glScalef(0.5f, 0.5f, 0.5f);
+            renderItem.renderItemModel(stack);
+            GL11.glPopMatrix();
         }
-        
-        GL11.glPopMatrix();
     }
 
 }
